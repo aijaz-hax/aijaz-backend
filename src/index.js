0e5535b4742113ea99cfad545b388e4f10a8e2dc
Aijaz_Bhat123 <aijaz.ahamed@reachiq.ai>
2024-07-04 16:02:39 +0530

    db connectionion done

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///  ONE APPROACH TO CONNECT ////
+
+
+// import express from "express"
+
+// const app = express()
+
+
+// ;(async()=>{
+//     try{
+//        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+//        app.on("error",(err)=>{
+//         console.log("ERR",err)
+//         throw err
+//        })
+
+//        app.listen(process.env.PORT, ()=>{
+//         console.log(`App is listening on port ${process.env.PORT}`)
+//        })
+
+//     }catch(err){
+//         console.log("ERR",err)
+//         throw err
+//     }
+// })()
